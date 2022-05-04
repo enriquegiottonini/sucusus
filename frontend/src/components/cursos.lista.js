64c30b6663e4b4a2b46ef75c062ac889f4d57f6d
migueln <miguelsn243@gmail.com>
2022-05-03 18:02:15 -0700

    lo mismo que el otro commit pero se me olvido cambiar un 3 por un 5

--- frontend/src/components/cursos.lista.js
+++ frontend/src/components/cursos.lista.js
@@ -62,7 +62,7 @@ const ListaCursos = () =>{
         setOffset(newoffset);
         getCursos(newoffset);
         setDisabled1(false);
-        if(newoffset+3>total) setDisabled2(true);
+        if(newoffset+5>total) setDisabled2(true);
     }
     const prevPage = () => {
         const newoffset = offset-5
